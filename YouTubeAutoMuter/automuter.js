ec88d25ee745548b94f784149391a91f7f7cf40b
TheNumberE <edwardx999@hotmail.com>
2017-07-14 22:48:28 -0400

    Made it work for any video on youtube (e.g. channel trailers)

--- YouTubeAutoMuter/automuter.js
+++ YouTubeAutoMuter/automuter.js
@@ -1,107 +1,109 @@
 console.log("AutoMuter Loaded");
-var adPlace=document.querySelector(".video-ads");
+var adPlace = document.querySelector(".video-ads");
 //console.log(adPlace);
-var MutationObserver=window.MutationObserver||window.WebKitMutationObserver;
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-var adObserver=new MutationObserver(
-    function() {
-        console.log("Ad Change Observed");
-        autoSkip();
-        autoMute();
-    });
+var adObserver = new MutationObserver(
+		function () {
+		console.log("Ad Change Observed");
+		autoSkip();
+		autoMute();
+	});
 
-if(thisIsAVideo()) {
-    console.log("Starting Observer");
-    adPlace=document.querySelector(".video-ads");
-    if(adPlace) {
-        adObserver.observe(adPlace,{
-            subtree: true,
-            childList: true
-        });
-    }
+var pollingInterval = 700;
+var maxAttempts = 3;
+var tryAgain = 0;
+var doubleCheckPage=true;
+setInterval(function () {
+	//console.log("Polling");
+	if(doubleCheckPage){
+		pageResponse();
+		doubleCheckPage=false;
+	}
+	else if (pageChanged()){
+		pageResponse();
+		doubleCheckPage=true;
+	}
+	else if (tryAgain > 0) {
+		--tryAgain;
+		adObserver.disconnect();
+		if (restartObserver())
+			tryAgain = 0;
+	}
+}, pollingInterval);
+
+function pageResponse() {
+	console.log("");
+	console.log("Change");
+	adObserver.disconnect();
+	if (thisIsAVideo()) {
+		console.log("Video Spotted");
+		tryAgain = (restartObserver()) ? 0 : maxAttempts;
+	}
 }
-
-var tryAgain=false;
-setInterval(function() {
-    //console.log("Polling");
-    if(pageChanged()) {
-        console.log("");
-        console.log("Change");
-        adObserver.disconnect();
-        if(thisIsAVideo()) {
-            console.log("Video Spotted");
-            tryAgain=!restartObserver();
-        }
-    }
-    else if(tryAgain) {
-        tryAgain=false;
-        adObserver.disconnect();
-        restartObserver();
-    }
-},700);
-
 function restartObserver() {
-    adPlace=document.querySelector(".video-ads");
-    if(adPlace!=null) {
-        console.log("Restarting Observer");
-        adObserver.observe(adPlace,{
-            subtree: true,
-            childList: true
-        });
-        return true;
-    }
-    console.log("Failed to find adPlace");
-    return false;
+	adPlace = document.querySelector(".video-ads");
+	if (adPlace != null) {
+		console.log("Restarting Observer");
+		adObserver.observe(adPlace, {
+			subtree: true,
+			childList: true
+		});
+		return true;
+	}
+	console.log("Failed to find adPlace");
+	return false;
 }
-var URL=window.location.href;
+var URL = window.location.href;
 function pageChanged() {
-    var before=URL;
-    return (URL=window.location.href)!=before;
+	var before = URL;
+	return (URL = window.location.href) != before;
 }
 
 function thisIsAVideo() {
-    return window.location.href.indexOf("watch?v")>-1;
+	//return -1<window.location.href.indexOf("watch?v");
+	return document.getElementsByClassName("html5-video-container").length > 0;
 }
 
-var shouldItBeMuted=false;
+var shouldItBeMuted = false;
 
 function isMuted() {
-    var volumeControls=document.getElementsByClassName("ytp-volume-panel");
-    if(volumeControls.length>0)
-        return volumeControls[0].getAttribute("aria-valuetext").indexOf("muted")>-1;
-    //console.log("Failed to find volume controls");
-    return true;
+	var volumeControls = document.getElementsByClassName("ytp-volume-panel");
+	if (0 < volumeControls.length)
+		return -1 < volumeControls[0].getAttribute("aria-valuetext").indexOf("muted");
+	//console.log("Failed to find volume controls");
+	return true;
 }
 
 function clickMuteButton() {
-    var muteButtons=document.getElementsByClassName("ytp-mute-button ytp-button");
-    if(muteButtons.length>0) {
-        document.getElementsByClassName("ytp-mute-button ytp-button")[0].click();
-        shouldItBeMuted=!shouldItBeMuted;
-    } //else
-    //console.log("Failed to find mute button");
+	var muteButtons = document.getElementsByClassName("ytp-mute-button ytp-button");
+	if (0 < muteButtons.length) {
+		document.getElementsByClassName("ytp-mute-button ytp-button")[0].click();
+		shouldItBeMuted = !shouldItBeMuted;
+	} //else
+	//console.log("Failed to find mute button");
 }
 
 function isAdPlaying() {
-    return 0<document.getElementsByClassName("videoAdUi").length;
+	return 0 < document.getElementsByClassName("videoAdUi").length;
 }
 
 function autoMute() {
-    if(shouldItBeMuted===isMuted()) {
-        if(isAdPlaying()) {
-            //console.log("Ad is Playing");
-            if(!isMuted())
-                clickMuteButton();
-        } else if(isMuted())
-            clickMuteButton();
-    }
+	if (shouldItBeMuted === isMuted()) {
+		if (isAdPlaying()) {
+			//console.log("Ad is Playing");
+			if (!isMuted())
+				clickMuteButton();
+		} else if (isMuted())
+			clickMuteButton();
+	}
 }
 
 function autoSkip() {
-    var skipButtons=document.getElementsByClassName("videoAdUiSkipButton videoAdUiAction videoAdUiFixedPaddingSkipButton");
-    if(0<skipButtons.length)
-        skipButtons[0].click();
-    var closeBanner=document.getElementsByClassName("close-button");
-    if(0<closeBanner.length)
-        closeBanner[0].click();
+	var skipButtons = document.getElementsByClassName("videoAdUiSkipButton videoAdUiAction videoAdUiFixedPaddingSkipButton");
+	if (0 < skipButtons.length)
+		skipButtons[0].click();
+	var closeBanner = document.getElementsByClassName("close-button");
+	if (0 < closeBanner.length)
+		closeBanner[0].click();
 }
